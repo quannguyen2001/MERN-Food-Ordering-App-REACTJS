4d82ee711f2a55cec3a8d912d9f86bcac8ddbb6f
Nguyễn An Quân <quannguyenthpttd@gmail.com>
2024-02-28 15:51:44 +0700

    first commit

---
+++ src/AppRouters.tsx
@@ -0,0 +1,22 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import Layout from "./layouts/layout";
+import HomePage from "./pages/HomePage";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
+import UserProfilePage from "./pages/UserProfilePage";
+import ProtectedRoute from "./auth/ProtectedRoute";
+
+
+const AppRouters = () => {
+    return (
+        <Routes>
+            <Route path="/auth-callback" element={<AuthCallbackPage />} />
+            <Route path="/" element={<Layout showHero><HomePage /></Layout>} />
+            <Route element={<ProtectedRoute />}>
+                <Route path="/user-profile" element={<Layout><UserProfilePage /></Layout>} />
+            </Route>
+            <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+    );
+};
+
+export default AppRouters
---
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -0,0 +1,48 @@
+import { Auth0Provider } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
+
+type Props = {
+    children: React.ReactNode;
+}
+
+const Auth0ProviderWithnavigate = ({ children }: Props) => {
+    // const { createUser } = useCreateMyUser();
+    const navigate = useNavigate();
+    // const { getAccessTokenSilently } = useAuth0();
+    const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+    const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+    const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+    const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+
+    if (!domain || !clientId || !redirectUri || !audience) {
+        throw new Error("Unable to initialise auth");
+    }
+
+    const onRedirectCallback = () => {
+        navigate("/auth-callback")
+    };
+
+    // const onRedirectCallback = async(appState?: AppState, user?: User) => {
+    //     // console.log("USER", user);
+    //     // if (user?.sub && user?.email) {
+    //     //     createUser({ auth0Id: user.sub, email: user.email });
+    //     // }
+    //     const token = await getAccessTokenSilently();
+    //     console.log("token",token);
+    //     navigate("/auth-callback")
+    // };
+
+    return (
+        <Auth0Provider
+            domain={domain}
+            clientId={clientId}
+            authorizationParams={{
+                redirect_uri: redirectUri,
+                audience,
+            }}
+            onRedirectCallback={onRedirectCallback}
+        >{children}</Auth0Provider>
+    )
+};
+
+export default Auth0ProviderWithnavigate;
