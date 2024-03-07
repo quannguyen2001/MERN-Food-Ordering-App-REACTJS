7bac7a085a821453b38a723d0c72c73048c4e932
Nguyễn An Quân <quannguyenthpttd@gmail.com>
2024-03-07 21:59:51 +0700

    Add search and filters

--- src/AppRouters.tsx
+++ src/AppRouters.tsx
@@ -5,12 +5,14 @@ import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import ManageRestaurantPage from "./pages/ManageRestaurantPage";
+import SearchPage from "./pages/SearchPage";
 
 
 const AppRouters = () => {
     return (
         <Routes>
             <Route path="/auth-callback" element={<AuthCallbackPage />} />
+            <Route path="/search/:city" element={<Layout showHero={false}><SearchPage /></Layout>} />
             <Route path="/" element={<Layout showHero><HomePage /></Layout>} />
             <Route element={<ProtectedRoute />}>
                 <Route path="/user-profile" element={<Layout><UserProfilePage /></Layout>} />
